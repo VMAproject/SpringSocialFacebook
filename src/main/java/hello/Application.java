9e2c276066ba852a65e3e1fb31bd07bf9525b671
Mixas <verlamov.m@gmail.com>
2016-12-01 15:32:19 +0200

    Test Spring Social Facebook

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,19 @@
+package hello;
+
+/**
+ * @Author Verlamov Mikhail
+ * @Git https://github.com/VMAproject
+ * @Email verlamov.m@gmail.com
+ * 01.12.2016
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
